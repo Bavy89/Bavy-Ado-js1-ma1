39d3a2cd55125657bb7bd5b97e4535905190d9dd
Bavy89 <85221462+Bavy89@users.noreply.github.com>
2022-03-30 14:24:07 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//question 1
+const animal = "cat";
+
+//question 2
+//question 3
+//question 4
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
