8a074177c745ec3eef7b7b40bc7b59dc7fc5c834
Bavy89 <85221462+Bavy89@users.noreply.github.com>
2022-04-06 15:37:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 //question 1
-const cat = {
-"complain": "meow!", 
-catComplain: function() 
-{
-console.log(this.complain);
-}
-}
+const cat = { 
+ complain:function complain (){ 
+ console.log("Meow!"); 
+ }, 
+ };
 
 //question 2
 
@@ -14,12 +12,29 @@ Heading.innerHTML = "Updated heading";
 
 //question 3
 
-const paragraph = document.querySelectorALL("p");
-
-paragraph.style.colour = "red"
-
+const sizeHeading = document.querySelector("h3");
+sizeHeading.style.fontSize = "2em";
 
 //question 4
+
+document.querySelector('h3').classList.add("subheading");
+
+
+//question 5
+
+const paragraphs = document.querySelectorAll("p");
+paragraphs.forEach((paragraph) => (paragraph.style.color = "red"));
+
+
+//question 6
+
+const resultsContainer = document.querySelector(".results");
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "New paragraph";
+resultsContainer.appendChild(newParagraph);
+resultsContainer.style.backgroundColor = "yellow";
+
+//question 7
 
 const cats = [
     {
@@ -35,7 +50,15 @@ const cats = [
     }
 ];
 
-//question 5
-//question 6
-//question 7
+function logNames(list) {
+for (let i = 0; i < list.length; i++) {
+console.log(list[i].name);
+}
+}
+
+logNames(cats);
+
 //question 8
+
+
+
