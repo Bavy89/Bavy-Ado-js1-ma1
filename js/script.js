28fe9abe9eb525c6a2f8b4cfbceef4f99f0de3cd
Bavy89 <85221462+Bavy89@users.noreply.github.com>
2022-04-08 12:32:06 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //question 1
 const cat = { 
+ 
  complain:function complain (){ 
  console.log("Meow!"); 
  }, 
@@ -8,11 +9,13 @@ const cat = {
 //question 2
 
 const Heading = document.querySelector("h3");
+
 Heading.innerHTML = "Updated heading";
 
 //question 3
 
 const sizeHeading = document.querySelector("h3");
+
 sizeHeading.style.fontSize = "2em";
 
 //question 4
@@ -23,6 +26,7 @@ document.querySelector('h3').classList.add("subheading");
 //question 5
 
 const paragraphs = document.querySelectorAll("p");
+
 paragraphs.forEach((paragraph) => (paragraph.style.color = "red"));
 
 
@@ -30,6 +34,7 @@ paragraphs.forEach((paragraph) => (paragraph.style.color = "red"));
 
 const resultsContainer = document.querySelector(".results");
 const newParagraph = document.createElement("p");
+
 newParagraph.innerText = "New paragraph";
 resultsContainer.appendChild(newParagraph);
 resultsContainer.style.backgroundColor = "yellow";
